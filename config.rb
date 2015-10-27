138dc42902d6051fea63dfa4e518b312151515cd
Opetus <kelli.haggett@gmail.com>
2015-10-27 15:02:29 -0500

    changed layout/finished

--- config.rb
+++ config.rb
@@ -2,4 +2,6 @@ require 'susy'
 css_dir = 'Code/css'
 sass_dir = 'Code/Working/sass'
 javascripts_dir = 'Code/js'
-output_style = :compressed
+#output_style = :compressed
+output_style = :expanded
+relative_assets = true
